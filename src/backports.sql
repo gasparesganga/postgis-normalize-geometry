48dba0e4e26b8e013196c832fac64d3d17476eba
Gaspare Sganga <contact@gasparesganga.com>
2020-05-27 19:21:29 +0200

    Fix multipolygon rebuilding

--- src/backports.sql
+++ src/backports.sql
@@ -1,10 +1,10 @@
-/* *************************************************************************************************
-NormalizeGeometry - PL/pgSQL function to remove spikes and simplify geometries with PostGIS
-    Author          : Gaspare Sganga
-    Version         : 1.2.0
-    License         : MIT
-    Documentation   : https://gasparesganga.com/labs/postgis-normalize-geometry/
-************************************************************************************************* */
+/*
+ *  NormalizeGeometry - PL/pgSQL function to remove spikes and simplify geometries with PostGIS.
+ *      Author          : Gaspare Sganga
+ *      Version         : 1.2.0
+ *      License         : MIT
+ *      Documentation   : https://gasparesganga.com/labs/postgis-normalize-geometry/
+ */
 
 
 /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -25,12 +25,14 @@ Notes:
     - The CASE statement is needed to correctly handle NULL values
 */
 CREATE OR REPLACE FUNCTION array_remove(
-    PAR_array   anyarray, 
-    PAR_element anyelement 
-) RETURNS anyarray AS $$
+      PAR_array     anyarray
+    , PAR_element   anyelement
+) RETURNS anyarray
+AS $$
     SELECT ARRAY(
-        SELECT value 
-            FROM unnest(PAR_array) AS value 
+        SELECT value
+            FROM unnest(PAR_array) AS value
             WHERE CASE WHEN PAR_element IS null THEN (value IS NOT null) ELSE (NOT value = PAR_element) END
     );
-$$ LANGUAGE sql;
+$$
+LANGUAGE sql;
--- src/normalize_geometry.sql
+++ src/normalize_geometry.sql
@@ -1,18 +1,19 @@
-/* *************************************************************************************************
-NormalizeGeometry - PL/pgSQL function to remove spikes and simplify geometries with PostGIS
-    Author          : Gaspare Sganga
-    Version         : 1.2.0
-    License         : MIT
-    Documentation   : https://gasparesganga.com/labs/postgis-normalize-geometry/
-************************************************************************************************* */
+/*
+ *  NormalizeGeometry - PL/pgSQL function to remove spikes and simplify geometries with PostGIS.
+ *      Author          : Gaspare Sganga
+ *      Version         : 1.2.0
+ *      License         : MIT
+ *      Documentation   : https://gasparesganga.com/labs/postgis-normalize-geometry/
+ */
 CREATE OR REPLACE FUNCTION normalize_geometry(
-    PAR_geom                        geometry, 
-    PAR_area_threshold              double precision, 
-    PAR_angle_threshold             double precision, 
-    PAR_point_distance_threshold    double precision, 
-    PAR_null_area                   double precision, 
-    PAR_union                       boolean DEFAULT true 
-) RETURNS geometry AS $$
+      PAR_geom                      geometry
+    , PAR_area_threshold            double precision
+    , PAR_angle_threshold           double precision
+    , PAR_point_distance_threshold  double precision
+    , PAR_null_area                 double precision
+    , PAR_union                     boolean DEFAULT true
+) RETURNS geometry
+AS $$
 -- Full documentation at https://gasparesganga.com/labs/postgis-normalize-geometry/
 DECLARE
     REC_linestrings record;
@@ -46,17 +47,17 @@ BEGIN
     
     ARR_output := '{}'::geometry[];
     FOR REC_linestrings IN EXECUTE $Q$
-        SELECT array_agg(COALESCE(ext_rings, (rdump).geom) ORDER BY (rdump).path[1]) AS geoms 
+        SELECT array_agg(COALESCE(ext_rings, (rdump).geom) ORDER BY (rdump).path[1]) AS geoms
             FROM (
-                SELECT row_number() OVER (PARTITION BY rings) AS r, COALESCE(rings, source) AS rdump 
-                    FROM                ST_Dump($1)                 AS source 
-                    LEFT JOIN LATERAL   ST_DumpRings(source.geom)   AS rings    ON $2 
-            ) AS d 
-            LEFT JOIN LATERAL ST_ExteriorRing((rdump).geom) AS ext_rings    ON $2 
-            GROUP BY r 
-    $Q$ USING PAR_geom, VAR_is_polygon LOOP 
+                SELECT COALESCE((source).path[1], 0) AS r, COALESCE(rings, source) AS rdump
+                    FROM                ST_Dump($1)                 AS source
+                    LEFT JOIN LATERAL   ST_DumpRings(source.geom)   AS rings    ON $2
+            ) AS d
+            LEFT JOIN LATERAL ST_ExteriorRing((rdump).geom) AS ext_rings ON $2
+            GROUP BY r
+    $Q$ USING PAR_geom, VAR_is_polygon LOOP
         ARR_parts := '{}'::geometry[];
-        FOREACH VAR_linestring IN ARRAY REC_linestrings.geoms LOOP 
+        FOREACH VAR_linestring IN ARRAY REC_linestrings.geoms LOOP
             VAR_tot := ST_NPoints(VAR_linestring);
             SELECT ST_IsClosed(VAR_linestring) INTO VAR_is_ring;
             IF VAR_is_ring THEN
@@ -67,7 +68,7 @@ BEGIN
                 VAR_old_tot := VAR_tot;
                 VAR_n       := 1;
                 WHILE VAR_n <= VAR_tot LOOP
-                    LOOP 
+                    LOOP
                         EXIT WHEN VAR_tot < 3 OR VAR_n > VAR_tot;
                         VAR_p0   := ST_PointN(VAR_linestring, CASE WHEN VAR_n = 1 THEN VAR_tot ELSE VAR_n - 1 END);
                         VAR_p1   := ST_PointN(VAR_linestring, VAR_n);
@@ -75,9 +76,9 @@ BEGIN
                         VAR_area := ST_Area(ST_MakePolygon(ST_MakeLine(ARRAY[VAR_p0, VAR_p1, VAR_p2, VAR_p0])));
                         IF VAR_area > PAR_null_area THEN
                             EXIT WHEN VAR_area > PAR_area_threshold;
-                            EXIT WHEN 
-                                (abs(pi() - abs(ST_Azimuth(VAR_p0, VAR_p1) - ST_Azimuth(VAR_p1, VAR_p2))) > PAR_angle_threshold) 
-                                AND (ST_Distance(VAR_p0, VAR_p1) > PAR_point_distance_threshold OR abs(pi() - abs(ST_Azimuth(VAR_p1, VAR_p2) - ST_Azimuth(VAR_p2, VAR_p0))) > PAR_angle_threshold) 
+                            EXIT WHEN
+                                    (abs(pi() - abs(ST_Azimuth(VAR_p0, VAR_p1) - ST_Azimuth(VAR_p1, VAR_p2))) > PAR_angle_threshold)
+                                AND (ST_Distance(VAR_p0, VAR_p1) > PAR_point_distance_threshold OR abs(pi() - abs(ST_Azimuth(VAR_p1, VAR_p2) - ST_Azimuth(VAR_p2, VAR_p0))) > PAR_angle_threshold)
                                 AND (ST_Distance(VAR_p1, VAR_p2) > PAR_point_distance_threshold OR abs(pi() - abs(ST_Azimuth(VAR_p2, VAR_p0) - ST_Azimuth(VAR_p0, VAR_p1))) > PAR_angle_threshold);
                         END IF;
                         VAR_linestring  := ST_RemovePoint(VAR_linestring, (CASE WHEN NOT VAR_is_polygon AND VAR_n = 1 AND VAR_area <= GREATEST(PAR_null_area, 0) THEN VAR_n ELSE VAR_n - 1 END));
@@ -87,10 +88,10 @@ BEGIN
                 END LOOP;
                 EXIT WHEN VAR_tot < 3 OR VAR_tot = VAR_old_tot;
             END LOOP;
-            IF VAR_is_ring THEN 
-                IF VAR_tot >= 3 THEN 
+            IF VAR_is_ring THEN
+                IF VAR_tot >= 3 THEN
                     ARR_parts := array_append(ARR_parts, ST_AddPoint(VAR_linestring, ST_PointN(VAR_linestring, 1)));
-                ELSIF NOT VAR_is_polygon THEN 
+                ELSIF NOT VAR_is_polygon THEN
                     ARR_parts := array_append(ARR_parts, VAR_linestring);
                 END IF;
             ELSE
@@ -104,14 +105,15 @@ BEGIN
         END IF;
     END LOOP;
     
-    IF PAR_union THEN 
+    IF PAR_union THEN
         SELECT ST_Union(ARR_output) INTO VAR_output;
     ELSE
         SELECT ST_Collect(ARR_output) INTO VAR_output;
-        IF ST_NumGeometries(VAR_output) = 1 THEN 
+        IF ST_NumGeometries(VAR_output) = 1 THEN
             SELECT (ST_Dump(VAR_output)).geom INTO VAR_output;
         END IF;
     END IF;
     RETURN VAR_output;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
